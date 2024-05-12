27653f00761221aaf5bcb49348cd46647b6502a6
serch <sergioibanezmoreno@gmail.com>
2024-05-12 14:32:08 +0200

    Loading images programatically

--- src/App.tsx
+++ src/App.tsx
@@ -12,46 +12,35 @@ import {
   useScroll,
 } from "@react-spring/web";
 
-import image1 from './static/informatica/190158.jpg'
-import image2 from './static/informatica/366210.jpg'
-import image3 from './static/informatica/436720.jpg'
-import image4 from './static/informatica/630732.jpg'
-import image5 from './static/informatica/266879FelixMorenoT04P58H01.jpg'
-import image6 from './static/informatica/366236.jpg'
-import image7 from './static/informatica/487606.jpg'
-import image8 from './static/informatica/653912.jpg'
-import image9 from './static/informatica/309490.jpg'
-import image10 from './static/informatica/373515.jpg'
-import image11 from './static/informatica/653918.jpg'
+// import image from './static/informatica/190158.jpg'
 
-const images = [
-  image1, 
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11
-]
+// const data = ["hi", "there!"];
 
-console.log(images[0])
+function importAll(r: any) {
+  let images = {} as any;
+  (r as any).keys().map((item: any, index: number) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  (require as any).context("./static/informatica", false, /\.(png|jpe?g|svg)$/)
+);
+
+console.log(images);
 
 const App = () => {
   const { scrollYProgress } = useScroll();
 
   return (
     <div className="wrapper">
-      <div className="grid-container">
-      {images.map((image, index) => {
+      {Object.keys(images).map((imageName, index) => {
+        console.log(images[imageName])
         return (
-          <img key={index} src={image} alt="jarl" />
+          <img key={index} src={images[imageName]} alt={imageName} />
         );
       })}
-      </div>
       <animated.div
         className="example"
         style={{ opacity: scrollYProgress.get() + 0.5 }}
