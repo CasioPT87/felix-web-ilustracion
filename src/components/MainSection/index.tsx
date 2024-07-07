3f7107b17a7a71a591e59d19f95e2bfd5e94a964
serch <sergioibanezmoreno@gmail.com>
2024-07-07 18:10:39 +0200

    WIP

--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -3,38 +3,48 @@ import "./styles.css";
 import { images } from "../../helper/imageLoader";
 import { getSortedPaths } from "../../helper/utils";
 import { MainSection } from "../types";
-import Pill from '../Pill'
-import Text from '../Text'
+import Pill from "../Pill";
+import Text from "../Text";
 import gifPortada from "../../static/videos/videosportada.gif";
 
 const mainSectionImages = Object.values(images.ilustraciones) as any[];
 
 const sortedImages = getSortedPaths(mainSectionImages) as any[];
 
-const Section = ({ type, pillText }: { type: MainSection, pillText: string }): any => {
+const Section = ({
+  type,
+  pillText,
+}: {
+  type: MainSection;
+  pillText: string;
+}): any => {
   if (type === "images") {
     return (
-      <>
-      <Pill top="6em"><p>{pillText}</p></Pill>
-      <div className="photo-container-ilustraciones">
-        {sortedImages.map((src, index) => (
-          <img key={index} src={src} className="photo-ilustraciones" />
-        ))}
+      <div className="main-section-container">
+        <Pill top="6em">
+          <p>{pillText}</p>
+        </Pill>
+        <div className="photo-container-ilustraciones">
+          {sortedImages.map((src, index) => (
+            <img key={index} src={src} className="photo-ilustraciones" />
+          ))}
+        </div>
+        <Text section={type} />
       </div>
-      <Text section={type} />
-      </>
     );
   } else if (type === "videos") {
     return (
-      <>
-      <Pill top="6em"><p>{pillText}</p></Pill>
-      <div className="gif-wrapper">
-        <div className="gif-container">
-          <img src={gifPortada} className="videoportada" />
+      <div className="main-section-container">
+        <Pill top="6em">
+          <p>{pillText}</p>
+        </Pill>
+        <div className="gif-wrapper">
+          <div className="gif-container">
+            <img src={gifPortada} className="videoportada" />
+          </div>
         </div>
+        <Text section={type} />
       </div>
-      <Text section={type} />
-      </>
     );
   }
   return null;
