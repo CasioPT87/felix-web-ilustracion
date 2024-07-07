db89bc2c2f7edf1cc5ae41dedee5598cb77d4d3a
serch <sergioibanezmoreno@gmail.com>
2024-07-07 10:28:24 +0200

    Refactoring

--- src/components/ImagesSection/index.tsx
+++ src/components/ImagesSection/index.tsx
@@ -4,7 +4,7 @@ import { animated, useTrail } from "@react-spring/web";
 import { images } from "../../helper/imageLoader";
 import { Section } from "../types";
 
-const NUM_COLUMNS = 4;
+const NUM_COLUMNS = 3;
 
 const belongsTo = (column: number, index: number): Boolean => {
   const round = Math.floor(index / NUM_COLUMNS);
@@ -35,46 +35,18 @@ const ImagesSection = ({ folderName }: { folderName: Section }) => {
   return (
     <div className="wrapper">
       <div className="grid-container">
-        <div className="children-row first">
-          {trails.map((style, index) => {
-            if (!belongsTo(0, index)) return null;
-            return (
-              <animated.div key={index} className="children" style={style}>
-                <img src={sectionImages[index]} />
-              </animated.div>
-            );
-          })}
-        </div>
-        <div className="children-row second">
-          {trails.map((style, index) => {
-            if (!belongsTo(1, index)) return null;
-            return (
-              <animated.div key={index} className="children" style={style}>
-                <img src={sectionImages[index]} />
-              </animated.div>
-            );
-          })}
-        </div>
-        <div className="children-row third">
-          {trails.map((style, index) => {
-            if (!belongsTo(2, index)) return null;
-            return (
-              <animated.div key={index} className="children" style={style}>
-                <img src={sectionImages[index]} />
-              </animated.div>
-            );
-          })}
-        </div>
-        <div className="children-row fourth">
-          {trails.map((style, index) => {
-            if (!belongsTo(3, index)) return null;
-            return (
-              <animated.div key={index} className="children" style={style}>
-                <img src={sectionImages[index]} />
-              </animated.div>
-            );
-          })}
-        </div>
+        {new Array(NUM_COLUMNS).fill(0).map((each, _index) => (
+          <div className="children-row">
+            {trails.map((style, index) => {
+              if (!belongsTo(_index, index)) return null;
+              return (
+                <animated.div key={index} className="children" style={style}>
+                  <img src={sectionImages[index]} />
+                </animated.div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
