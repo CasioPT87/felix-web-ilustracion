205844172658f3973be9f18ddf072f84855a3a1a
serch <sergioibanezmoreno@gmail.com>
2024-07-07 10:16:47 +0200

    Images fitting perfectly

--- src/components/ImagesSection/index.tsx
+++ src/components/ImagesSection/index.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { animated, useTrail } from "@react-spring/web";
-import { images } from '../../helper/imageLoader'
-import { Section } from '../types'
+import { images } from "../../helper/imageLoader";
+import { Section } from "../types";
 
-const isHorizontal = (img: { width: number; height: number }): Boolean => {
-  return (img.width / img.height) > 1.5;
+const NUM_COLUMNS = 4;
+
+const belongsTo = (column: number, index: number): Boolean => {
+  const round = Math.floor(index / NUM_COLUMNS);
+  return column === index - round * NUM_COLUMNS;
 };
 
 const ImagesSection = ({ folderName }: { folderName: Section }) => {
-
-  const [imageShapeMap, setImageShapeMap]: any = useState({});
   const [sectionImages, setSectionImages] = useState([]);
 
   const trails = useTrail(sectionImages.length, {
@@ -21,33 +22,59 @@ const ImagesSection = ({ folderName }: { folderName: Section }) => {
 
   useEffect(() => {
     if (images[folderName]) {
-      const imagesToSet = Object.values(images[folderName]) as any
+      const imagesToSet = Object.values(images[folderName]) as any;
       setSectionImages(imagesToSet);
-  } else {
-      console.error('Folder not found:', folderName);
+    } else {
+      console.error("Folder not found:", folderName);
       setSectionImages([]);
-  }
-}, [folderName]);
+    }
+  }, [folderName]);
 
-  if (!sectionImages.length) return null
+  if (!sectionImages.length) return null;
 
   return (
     <div className="wrapper">
       <div className="grid-container">
-        {trails.map((style, index) => (
-          <animated.div key={index} className="children" style={style}>
-            <img
-              onLoad={(imgObj) => {
-                const target = imgObj.target as HTMLImageElement
-                setImageShapeMap((map: any) => ({
-                  ...map,
-                  [index]: isHorizontal(target),
-                }));
-              }}
-              src={sectionImages[index]}
-            />
-          </animated.div>
-        ))}
+        <div className="children-row first">
+          {trails.map((style, index) => {
+            if (!belongsTo(0, index)) return null;
+            return (
+              <animated.div key={index} className="children" style={style}>
+                <img src={sectionImages[index]} />
+              </animated.div>
+            );
+          })}
+        </div>
+        <div className="children-row second">
+          {trails.map((style, index) => {
+            if (!belongsTo(1, index)) return null;
+            return (
+              <animated.div key={index} className="children" style={style}>
+                <img src={sectionImages[index]} />
+              </animated.div>
+            );
+          })}
+        </div>
+        <div className="children-row third">
+          {trails.map((style, index) => {
+            if (!belongsTo(2, index)) return null;
+            return (
+              <animated.div key={index} className="children" style={style}>
+                <img src={sectionImages[index]} />
+              </animated.div>
+            );
+          })}
+        </div>
+        <div className="children-row fourth">
+          {trails.map((style, index) => {
+            if (!belongsTo(3, index)) return null;
+            return (
+              <animated.div key={index} className="children" style={style}>
+                <img src={sectionImages[index]} />
+              </animated.div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
