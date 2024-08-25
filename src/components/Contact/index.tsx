0595a1fd6b586e7935e1b1288a514a9e796f1625
serch <sergioibanezmoreno@gmail.com>
2024-08-25 19:12:06 +0200

    Improving Contact styles

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,16 +3,16 @@ import monkey from "../../static/contacto/monkey.jpg";
 import Pill from "../Pill";
 import "./styles.css";
 
-const Contact = function() {
+const Contact = function () {
   return (
     <>
-      <Pill top='6em'><p>Contacto</p></Pill>
+      <Pill top="6em">
+        <p>Contacto</p>
+      </Pill>
       <div className="contact-container">
-        <div className="contact-column">moreno.felix@gmail.com</div>
-        <div className="contact-column">
-          <img src={monkey} className="monkey" />
-        </div>
-        <div className="contact-column">+34 699 86 79 51</div>
+        <p>moreno.felix@gmail.com</p>
+        <p>+34 699 86 79 51</p>
+        <img src={monkey} className="monkey" />
       </div>
     </>
   );
